7fb8d185f9e70a68e803bd1c3770bb684113ffd4
moh <osman.elrashid@gmail.com>
2019-08-02 04:44:39 +0400

    initial files

---
+++ android/app/src/main/java/com/example/e013_flutter_slivergrid_e004base/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.e013_flutter_slivergrid_e004base;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
